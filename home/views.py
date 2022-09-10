78c12c40b84b1b689f41482f9c18fff801941c71
sh4d0wl3ss <adam@adampressman.com>
2022-09-10 15:40:20 -0400

    Through Exercises 8 done.

--- home/views.py
+++ home/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html' 
--- notes/forms.py
+++ notes/forms.py
@@ -16,8 +16,4 @@ class NotesForm(forms.ModelForm):
             'text': 'Write your thoughts here:'
         }
 
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if 'Django' not in title:
-            raise ValidationError('We only accept notes about Django!')
-        return title
+
